a75865b07899b4517d31c7fd2bf9367b28734d30
fkei753 <f.keishiro0305@gmail.com>
2025-07-26 15:01:52 +0900

    Update index.tsx

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div>
-      <h1>ようこそ</h1>
-      <p>これはPlaywrightハンズオン用のサンプルアプリです。</p>
-    </div>
-  );
-}
+    <>
+      <Head>
+        <title>Sample App</title>
+      </Head>
+      <div>
+        <h1>ようこそ</h1>
+        <p>これはPlaywrightハンズオン用のサンプルアプリです。</p>
+      </div>
+    </>
+  )
+} 
