5c43acfa91ae585920154be5231a1d2d2463c35f
fkei753 <f.keishiro0305@gmail.com>
2025-07-26 11:15:32 +0900

    Delete app/tests directory

--- app/tests/sample.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('トップページが正しく表示される', async ({ page }) => {
-  await page.goto('/');
-  await expect(page).toHaveTitle(/Sample App/);
-  await expect(page.locator('h1')).toContainText('ようこそ');
-});
